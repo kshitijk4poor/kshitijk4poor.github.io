500204a793f0228d8019be68c9489e9fc074d4c2
kshitij <82637225+kshitijk4poor@users.noreply.github.com>
2024-11-26 14:50:12 +0530

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://kshitijk.com",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: vercel(),
